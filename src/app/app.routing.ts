3bf16d14daf0b4b156d45c693a35230ffcc3bf2a
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-07-23 16:13:23 -0700

    will now allow user to view all listed caches

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddGeoSiteComponent } from './add-geo-site/add-geo-site.component';
+import { CacheListingsComponent } from './cache-listings/cache-listings.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: AddGeoSiteComponent
+  },
+  {
+    path: 'app-cache-listings',
+    component: CacheListingsComponent
   }
 
 ];
